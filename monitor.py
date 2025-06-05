26b10d55a3d7d5cc1e80ed0af83e04c9dfa17099
gregorywargnier <gregory.wargnier60@gmail.com>
2025-06-05 14:31:13 +0200

    chore : create file json and log

--- monitor.py
+++ monitor.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import dotenv
@@ -6,11 +7,11 @@ import os
 
 
 logging.basicConfig(
-    level=logging.INFO, # Jusque quel level on affiche les logs
-    format='%(asctime)s [%(levelname)s] %(message)s Data: %(args)s', # Format des logs
-    handlers=[ # Quelles sont les destinations des logs
-        # logging.StreamHandler(), # Affichage dans la console
-        logging.FileHandler('log.log'), # Affichage dans un fichier
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s Data: %(args)s',
+    handlers=[
+         logging.StreamHandler(),
+        logging.FileHandler('logs/app.log'),
     ],
 )
 
@@ -23,15 +24,37 @@ API_URL = os.getenv("API_URL")
 headers = {
     "Authorization": f"Bearer {API_TOKEN}"
 }
-timeout=0.5
+timeout=5
 
 def fast_api():
     try:
+        #Ne pas afficher le token
+        token = API_TOKEN
+        hide = token.replace(token, '***')
 
         response = requests.get(f'{API_URL}',
                                 headers=headers,
                                 timeout=timeout)
 
+        # json save
+
+        result = response.json()
+
+        data = "result.json"
+
+        dossier = "reports"
+
+        os.makedirs(dossier, exist_ok=True)
+
+        route = os.path.join(dossier, data)
+
+        # Sauvegarde des données en format JSON
+        with open(route, 'w', encoding='utf-8') as f:
+            json.dump(result, f, indent=4, ensure_ascii=False)
+
+        print(f"Résultats sauvegardés dans le dossier {route}")
+
+        logging.info(f'HTTP response from {API_URL},{hide} with status code {response.status_code}')
         response.raise_for_status()
         if 500 < response.status_code <= 599:
             raise Exception(f'Erreur {response.status_code} sur l\'API {response.reason}')
@@ -40,6 +63,8 @@ def fast_api():
             raise Exception(f'Erreur {response.status_code} sur l\'API {response.reason}')
 
         return response
+    except requests.exceptions.HTTPError as e:
+        logging.error(f'HTTP error: {hide}')
     except Exception as e:
         logging.error(e.args[0])
         return 'Erreur sur l\'API'
